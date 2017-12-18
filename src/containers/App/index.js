eae4f8c20465365719b023fc83e62e30fe54b57e
Gideon Kadzura <gideonkadzura@gmail.com>
2017-12-18 11:53:12 +0000

    Register route

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,31 +1,25 @@
 /**
  *
  * App - Personal Dashboard
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- */
+ **/
 import React, { Component } from "react";
 import LandingPage from "../LandingPage";
 import PersonalAccount from "../PersonalAccount";
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
+import RegisterPage from "../RegisterPage";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class PersonalAccountApp extends Component {
-	render() {
-		return (
-			<Router>
-				<div>
-					<Route exact path="/" component={LandingPage} />
-
-					<Route path="/personalaccount" component={PersonalAccount} />
-
-				</div>
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/register" component={RegisterPage} />
+          <Route path="/personalaccount" component={PersonalAccount} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default PersonalAccountApp;
---
+++ src/containers/RegisterPage/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class RegisterPage extends Component {
+  render() {
+    return <div>register</div>;
+  }
+}
+
+export default RegisterPage;
