a129e2d2b909d15ba8ff4b1118425314fe79786c
Gideon Kadzura <gideonkadzura@gmail.com>
2017-12-18 13:36:03 +0000

    Registration

--- src/containers/RegisterPage/index.js
+++ src/containers/RegisterPage/index.js
@@ -4,6 +4,7 @@ import BrandLogo from "../../components/BrandImg";
 
 const AuthenticationContianer = styled.div`
   display: grid;
+  grid-template-rows: 80px 200px 100px 300px;
   justify-items: center;
   background-color: #6ca516;
   height: 100vh;
@@ -20,12 +21,9 @@ const ButtonLanding = styled.button`
   border: 0px;
   border-radius: 8px;
   border-bottom: 3px solid #6f6d6f;
-  color: #ffffff;
-  font-family: Campton-BoldDEMO;
-  font-size: 12px;
-  letter-spacing: 1.37px;
   width: 237px;
   height: 45px;
+  margin-bottom: 28px;
   ${props =>
     props.primary &&
     css`
@@ -45,22 +43,90 @@ const ButtonLanding = styled.button`
       border-bottom: 3px solid #224899;
     `};
 `;
+const Icon = styled.img`
+  float: left;
+  margin-left: 11px;
+`;
+const MailText = styled.p`
+  display: inline-block;
+  margin: 0px;
+  padding-top: 1px;
+  color: #ffffff;
+  font-family: Campton-Bold;
+  font-size: 12px;
+  letter-spacing: 1.37px;
+  margin-right: 32px;
+`;
+const BrandContianer = styled.div`
+  align-self: end;
+`;
+const Terms = styled.p`
+  font-family: Campton-Book;
+  font-size: 9px;
+  color: #ffffff;
+  letter-spacing: 0;
+  line-height: 24px;
+  text-align: center;
+`;
+
+const SubFormText = styled.p`
+  font-family: Campton-Book;
+  font-size: 13px;
+  color: #ffffff;
+  letter-spacing: 0;
+  text-align: center;
+`;
+const ButtonContainer = styled.div`
+  display: grid;
+  justify-items: center;
+`;
+
+const CancelContainer = styled.div`
+  display: grid;
+  justify-self: end;
+  align-self: center;
+  margin-right: 100px;
+`;
+const Cancel = styled.div``;
+const CancelImg = styled.img``;
 
 class RegisterPage extends Component {
   render() {
     return (
       <AuthenticationContianer>
-        <div>
+        <CancelContainer>
+          <Cancel>
+            <CancelImg src={require("../../components/Icons/Cancel.svg")} />
+          </Cancel>
+        </CancelContainer>
+        <BrandContianer>
           <BrandLogo />
-        </div>
+        </BrandContianer>
         <div>
           <Header>Register</Header>
         </div>
-        <div>
-          <ButtonLanding primary>MAIL</ButtonLanding>
-          <ButtonLanding google>GOOGLE</ButtonLanding>
-          <ButtonLanding facebook>FACEBOOK</ButtonLanding>
-        </div>
+        <ButtonContainer>
+          <ButtonLanding primary>
+            <Icon src={require("../../components/Icons/Mail.svg")} />
+            <MailText>MAIL</MailText>
+          </ButtonLanding>
+          <ButtonLanding google>
+            <Icon src={require("../../components/Icons/Google.svg")} />
+            <MailText>GOOGLE</MailText>
+          </ButtonLanding>
+          <ButtonLanding facebook>
+            <Icon src={require("../../components/Icons/Facebook.svg")} />
+            <MailText>FACEBOOK</MailText>
+          </ButtonLanding>
+          <div>
+            <SubFormText>
+              Already registered please <span>sign me in</span>
+            </SubFormText>
+            <Terms>
+              By contining, you accept the terms of use and Privacy Policy
+            </Terms>
+          </div>
+        </ButtonContainer>
       </AuthenticationContianer>
     );
   }
