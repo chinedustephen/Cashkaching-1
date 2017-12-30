239121bcd3fbc8cee3db3223bab3a957d18a862d
Gideon Kadzura <gideonkadzura@gmail.com>
2017-12-30 03:39:42 +0000

    Timer layout reverted to normal statless function

--- src/components/LandingPageComponents/SectionOne/HeaderTimerSection/index.js
+++ src/components/LandingPageComponents/SectionOne/HeaderTimerSection/index.js
@@ -5,29 +5,18 @@ import TimerHeaderImg from "./TimerImg";
 import TimerCountDown from "./TimerText";
 import Linktext from "./LinkText";
 
-const Timer = styled.div`
+const TimerLayout = styled.div`
   display: grid;
   justify-items: center;
 `;
 
-const TimerText = styled.p`
-  font-family: Roboto-Medium;
-  font-size: 16px;
-  color: #575555;
-  line-height: 24px;
-
-  text-align: center;
-  grid-row-start: 3;
-  grid-row-end: span 1;
-`;
-
 const TimerHeaderContent = () => (
-  <Timer>
+  <TimerLayout>
     <TimerHeader timerHeader="timer header" />
     <TimerHeaderImg />
     <TimerCountDown time="40:30" />
     <Linktext text="text test" />
-  </Timer>
+  </TimerLayout>
 );
 
 export default TimerHeaderContent;
