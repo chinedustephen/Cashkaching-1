4590ee33b199322d7475a8120f8530f99cc0ac50
Gideon Kadzura <gideonkadzura@gmail.com>
2018-01-07 09:01:00 +0000

    Advert card styling complete

--- src/components/AdvertCard/index.js
+++ src/components/AdvertCard/index.js
@@ -23,23 +23,33 @@ const Cardlayout = styled.div`
 const CardContent = styled.div``;
 
 const CardLabel = styled.div`
+  display: grid;
+  grid-template-columns: 78% 1fr;
   grid-column-start: 1;
   grid-column-end: 4;
+  margin-left: 24px;
 `;
 
 const AdTextContianer = styled.div`
   display: grid;
+  grid-template-rows: 1fr 1fr 1fr;
 `;
 
 const ConentContainer = styled.div`
   display: grid;
   grid-template-columns: 50% 50%;
+  margin-top: 48px;
+  margin-left: 24px;
+  margin-right: 24px;
+  margin-bottom: 24px;
 `;
 
 const SponsoredText = styled.p`
   font-family: Campton-Book;
   font-size: 12px;
   color: #fafafa;
+  margin-top: 18px;
+  margin-bottom: 0px;
 `;
 
 const SkipText = styled.p`
@@ -49,19 +59,8 @@ const SkipText = styled.p`
   color: #ffffff;
   letter-spacing: 0;
   line-height: 17px;
-`;
-const AdCounter = styled.p`
-  font-family: Campton-Book;
-  font-size: 12px;
-  color: #fafafa;
-  letter-spacing: 0.16px;
-`;
-
-const AdImgTest = styled.div`
-  width: 281px;
-  height: 221px;
-  background: #6ca516;
-  background-image: url(${Ad});
+  margin-top: 18px;
+  margin-bottom: 0px;
 `;
 
 const AdBonusText = styled.p`
@@ -78,6 +77,7 @@ const BonusBadge = styled.div`
   border-radius: 4px;
   height: 21px;
   display: inline-block;
+  padding: 3px;
 `;
 const AdText = styled.p`
   /* Like what you see do: */
@@ -101,55 +101,83 @@ const BonusFigure = styled.p`
   font-size: 10px;
   color: #000000;
   letter-spacing: 0.13px;
+  margin: 0px;
+  margin-top: 5px;
+  margin-left: 5px;
+  margin-right: 5px;
+`;
+const SkipAdContianer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 51px;
+`;
+const Next = styled.img``;
+const NextContainer = styled.div`
+  align-self: center;
+  margin-top: 13px;
 `;
 
-class AdvertCard extends Component {
-  render() {
-    return (
-      <Cardlayout>
-        <Cardcontainer>
-          <CardContent>
-            <CardLabel>
-              <div>
-                <SponsoredText>Sponsored Ad</SponsoredText>
-              </div>
-              <div>
-                <SkipText>Skip to free Lotto</SkipText>
-              </div>
-            </CardLabel>
-            <ConentContainer>
-              <div>
-                <div>
-                  counter
-                  <AdCounter>0:15</AdCounter>
-                </div>
-                <div>
-                  <AdImgTest />
-                </div>
-              </div>
-              <AdTextContianer>
-                <div>
-                  <AdBonusText>Play this sponsord ad and earn</AdBonusText>
-                  <BonusBadge>
-                    <BonusFigure>+ N 50</BonusFigure>
-                  </BonusBadge>
-                </div>
-                <div>
-                  <AdText>
-                    Like what you see dont wait For more information please vist
-                    Learn more.
-                  </AdText>
-                </div>
-                <div>
-                  <TermsConditions>Terms and conditions apply</TermsConditions>
-                </div>
-              </AdTextContianer>
-            </ConentContainer>
-          </CardContent>
-        </Cardcontainer>
-      </Cardlayout>
-    );
-  }
-}
+const AdImage = styled.img``;
+const BonusPointsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 30%;
+`;
+
+const TextContainer = styled.div`
+  align-self: center;
+`;
+const BadgeContainer = styled.div`
+  align-self: center;
+  justify-self: end
+  margin-bottom: 4px;
+
+`;
+
+const AdvertCard = () => (
+  <Cardlayout>
+    <Cardcontainer>
+      <CardContent>
+        <CardLabel>
+          <div>
+            <SponsoredText>Sponsored Ad</SponsoredText>
+          </div>
+          <SkipAdContianer>
+            <SkipText>Skip to free Lotto</SkipText>
+            <NextContainer>
+              <Next src={require("./images/next.svg")} />
+            </NextContainer>
+          </SkipAdContianer>
+        </CardLabel>
+        <ConentContainer>
+          <div>
+            <div>
+              <AdImage src={require("./images/img.svg")} />
+            </div>
+          </div>
+          <AdTextContianer>
+            <BonusPointsContainer>
+              <TextContainer>
+                <AdBonusText>Play this sponsord ad and earn</AdBonusText>
+              </TextContainer>
+              <BadgeContainer>
+                <BonusBadge>
+                  <BonusFigure>+ N 50</BonusFigure>
+                </BonusBadge>
+              </BadgeContainer>
+            </BonusPointsContainer>
+            <div>
+              <AdText>
+                Like what you see dont wait For more information please vist
+                Learn more.
+              </AdText>
+            </div>
+            <div>
+              <TermsConditions>Terms and conditions apply</TermsConditions>
+            </div>
+          </AdTextContianer>
+        </ConentContainer>
+      </CardContent>
+    </Cardcontainer>
+  </Cardlayout>
+);
 
 export default AdvertCard;
